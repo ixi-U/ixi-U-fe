0913df9fcd9415e9534dee34e0cf2fd2c1ced311
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-06-17 13:34:25 +0900

    fix : SCRUM-58  App.js Merge Confilict 수정

--- src/App.js
+++ src/App.js
@@ -16,13 +16,13 @@ import ChatBotPage from './components/ChatBotPage';
 import OnBoarding from "./pages/OnBoarding";
 import Logout from "./pages/Logout";
 
-function AppContent() {
-  const location = useLocation();
-  // const hideHeaderRoutes = ["/", "/onboarding"]; # Header 적용안하는 페이지
+function App() {
+  // const location = useLocation();
+  // const hideHeaderRoutes = ["/", "/onboarding"]; // Header 적용안하는 페이지
   // const shouldHideHeader = hideHeaderRoutes.includes(location.pathname);
 
   return (
-    <>
+    <Router>
       {/* {!shouldHideHeader && <Header />} */}
       <Routes>
         <Route path="/" element={<SocialLogin />} />
@@ -32,25 +32,9 @@ function AppContent() {
         <Route path="/onboarding" element={<OnBoarding />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/chatbot" element={<ChatBotPage />} />
-      </Routes>
-    </>
-  );
-}
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login/status" element={<LoginStatus />} />{" "}
-        {/* 서버에서 리디렉션하는 경로 */}
-        <Route path="/mainPage" element={<MainPage />} />{" "}
-        {/* 로그인 성공 후 이동할 메인 페이지 */}
-        <Route path="/plans" element={<PlanListPage />} />
         <Route path="/plans/details/:planId" element={<PlanDetailPage />} />
         <Route path="/mypage" element={<MyPage />} />
       </Routes>
-      <AppContent />
     </Router>
   );
 }
