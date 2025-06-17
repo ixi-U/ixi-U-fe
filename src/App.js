e8d846168b7b3fbd85c73adf65f3ea00382f8181
yereumi <lovelyyeji01@gmail.com>
2025-06-17 19:12:48 +0900

    fix: SCRUM-75 Router 수정

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import {
   Route,
   useLocation,
 } from "react-router-dom";
-import Header from "./components/Header";
-import SocialLogin from "./pages/SocialLogin";
-import LoginStatus from "./pages/LoginStatus";
+import Header from "./components/header/Header";
+import SocialLogin from "./pages/login/SocialLogin";
+import LoginStatus from "./pages/login/LoginStatus";
 import MainPage from "./pages/MainPage";
-import PlanListPage from './components/PlanListPage';
-import PlanDetailPage from './components/PlanDetailPage';
-import ChatBotPage from './components/ChatBotPage';
-import OnBoarding from "./pages/OnBoarding";
-import Logout from "./pages/Logout";
-import SingleBenefitRegisterForm from "./components/SingleBenefitRegisterForm";
-import BundledBenefitRegisterForm from "./components/BundledBenefitRegisterForm";
+import PlanListPage from "./pages/plans/plan-list/PlanListPage";
+import PlanDetailPage from "./pages/plans/plan-detail/PlanDetailPage";
+import ChatBotPage from "./pages/chatbot/ChatBotPage";
+import OnBoarding from "./pages/login/OnBoarding";
+import Logout from "./pages/login/Logout";
+import SingleBenefitRegisterForm from "./pages/admin/SingleBenefitRegisterForm";
+import BundledBenefitRegisterForm from "./pages/admin/BundledBenefitRegisterForm";
 
 function AppContent() {
   const location = useLocation();
@@ -26,12 +26,14 @@ function AppContent() {
     <>
       {/* {!shouldHideHeader && <Header />} */}
       <Routes>
-        <Route path="/" element={<SocialLogin />} />
-        <Route path="/login/status" element={<LoginStatus />} />
-        <Route path="/mainPage" element={<MainPage />} />
-        <Route path="/plans" element={<PlanListPage />} />
-        <Route path="/onboarding" element={<OnBoarding />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<SocialLogin />} />
         <Route path="/logout" element={<Logout />} />
+        <Route path="/login/status" element={<LoginStatus />} />
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/plans" element={<PlanListPage />} />
+        <Route path="/plans/details" element={<PlanDetailPage />} />
+        <Route path="/onboarding" element={<OnBoarding />} />
         <Route path="/chatbot" element={<ChatBotPage />} />
         <Route path="/admin/benefits/single-benefits" element={<SingleBenefitRegisterForm/>}/>
         <Route path="/admin/benefits/bundled-benefits" element={<BundledBenefitRegisterForm/>}/>
