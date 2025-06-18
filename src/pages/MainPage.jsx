8c81638f287e288d226561fa15f9cee3f09c1d38
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-06-18 14:28:09 +0900

    fix: SCRUM-65 main navigate 수정

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -5,25 +5,26 @@ import Header from "../components/header/Header";
 
 export default function MainPage() {
   return (
-    <main className="main-wrapper">
+    <main className="container">
       {/* 전역 헤더 */}
       <Header />
+      <div className="main-wrapper">
+        {/* 메인 히어로 영역 */}
+        <section className="hero-section">
+          <button className="nav-arrow left">❮</button>
 
-      {/* 메인 히어로 영역 */}
-      <section className="hero-section">
-        <button className="nav-arrow left">❮</button>
+          <div className="hero-content">
+            <img src={centerLogo} alt="중앙 로고" className="hero-logo" />
+            <p className="hero-text">
+              U+의 다양한 요금제를
+              <br />
+              자신의 필요에 맞게 탐색하세요
+            </p>
+          </div>
 
-        <div className="hero-content">
-          <img src={centerLogo} alt="중앙 로고" className="hero-logo" />
-          <p className="hero-text">
-            U+의 다양한 요금제를
-            <br />
-            자신의 필요에 맞게 탐색하세요
-          </p>
-        </div>
-
-        <button className="nav-arrow right">❯</button>
-      </section>
+          <button className="nav-arrow right">❯</button>
+        </section>
+      </div>
     </main>
   );
 }
