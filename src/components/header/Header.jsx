304521a3fb6715d6a3fc9ed47428182225fe6950
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-06-20 14:25:52 +0900

    feat : SCRUM-108 로그인 버튼 옆에 마이페이지 모양 그림 삭제

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -83,11 +83,6 @@ const Header = () => {
       </div>
       {!isLoginPage && !isLoading && (
         <div className="header-user" style={{ display: 'flex', alignItems: 'center', gap: 16 }}>
-          <span className="user-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="user-svg">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.5 20.25a8.25 8.25 0 1115 0v.75a.75.75 0 01-.75.75H5.25a.75.75 0 01-.75-.75v-.75z" />
-            </svg>
-          </span>
           {isLoggedIn ? (
             <button onClick={handleLogout} className="login-btn">로그아웃</button>
           ) : (
